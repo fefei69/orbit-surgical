114a7ef46bb66f5d9afaf0c5c3fa577d4f3212e5
fefei69 <coolpixs04@gmail.com>
2025-03-16 22:58:13 +0000

    small update

--- run_code.sh
+++ run_code.sh
@@ -1,3 +1,3 @@
-# ${IsaacLab_PATH}/isaaclab.sh -p source/standalone/workflows/robomimic/eval_act.py --num_envs 1 --enable_cameras --livestream 2
+${IsaacLab_PATH}/isaaclab.sh -p source/standalone/workflows/robomimic/eval_act.py --num_envs 1 --enable_cameras --livestream 2
 # ${IsaacLab_PATH}/isaaclab.sh -p source/standalone/workflows/robomimic/test_state_machine.py --num_envs 1 --enable_cameras --livestream 2
-${IsaacLab_PATH}/isaaclab.sh -p source/standalone/workflows/robomimic/test.py --num_envs 1 --enable_cameras --livestream 2
+# ${IsaacLab_PATH}/isaaclab.sh -p source/standalone/workflows/robomimic/test.py --num_envs 1 --enable_cameras --livestream 2
